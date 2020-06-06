30a42268551713a812deebe1ebf3b530eeb5ee44
Nikhil Singh <nikhil.singh@olx.com>
2020-06-06 17:28:23 +0530

    Added Reading Quote Functionality

--- app/src/main/java/com/ronan/kowts/widgets/RonanQuoteMenuBottomSheet.kt
+++ app/src/main/java/com/ronan/kowts/widgets/RonanQuoteMenuBottomSheet.kt
@@ -1,18 +1,25 @@
 package com.ronan.kowts.widgets
 
 import android.os.Bundle
+import android.speech.tts.TextToSpeech
+import android.speech.tts.TextToSpeech.OnInitListener
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.ronan.kowts.R
 import com.ronan.kowts.actions.RonanQuoteBottomSheetAction
-import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.ronan_quote_menu_bottom_sheet.*
+import java.util.*
 
-class RonanQuoteMenuBottomSheet() : BottomSheetDialogFragment() {
+
+class RonanQuoteMenuBottomSheet() : BottomSheetDialogFragment(), TextToSpeech.OnInitListener {
 
     var actionListener: ActionListener? = null
     lateinit var fullQuoteText: String
+    private var tts: TextToSpeech? = null
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -22,8 +29,14 @@ class RonanQuoteMenuBottomSheet() : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        tts = TextToSpeech(context, this)
+
         ivCloseBottomSheet.setOnClickListener {
             dismiss()
+        }
+
+        tvReadQuote.setOnClickListener {
+            tts!!.speak(fullQuoteText, TextToSpeech.QUEUE_FLUSH, null,"")
         }
         tvCopyQuote.setOnClickListener {
             actionListener?.onActionListener(RonanQuoteBottomSheetAction.COPY_QUOTE, fullQuoteText)
@@ -41,6 +54,28 @@ class RonanQuoteMenuBottomSheet() : BottomSheetDialogFragment() {
 
     interface ActionListener {
         fun onActionListener(action: RonanQuoteBottomSheetAction, fullQuoteText: String)
+    }
+
+    override fun onInit(status: Int) {
+        if (status == TextToSpeech.SUCCESS) {
+            val result = tts!!.setLanguage(Locale.UK)
+
+            if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
+                Log.e("TTS","The Language specified is not supported!")
+            } else {
+            }
+
+        } else {
+            Log.e("TTS", "Initilization Failed!")
+        }
+    }
+
+    override fun onDestroy() {
+        if (tts != null) {
+            tts!!.stop()
+            tts!!.shutdown()
+        }
+        super.onDestroy()
     }
 
 }
